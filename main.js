6ef25c41eaa8bf2475e5d5d551094195273954fa
Giovanna <elisangela.colombo72@gmail.com>
2026-07-07 08:42:04 -0300

    Update main.js

--- main.js
+++ main.js
@@ -4,11 +4,11 @@ numeroSenha.textContent = tamanhoSenha;
 const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVXYWZ';
 const letrasMinusculas = 'abcdefghijklmnopqrstuvxywz';
 const numeros = '0123456789';
-const simbolos = '!@%*?';
-const botoes = document.querySelectorAll('.parametro-senha__botao');
-const campoSenha = document.querySelector('#campo-senha');
-const checkbox = document.querySelectorAll('.checkbox');
-const forcaSenha = document.querySelector('.forca');
+const simbolos = '!@#$%^&*()-+?';
+const botoes = document.querySelectorAll(".parametro-senha__botao");
+const campoSenha = document.querySelector("#campo-senha");
+const checkbox = document.querySelectorAll(".checkbox");
+const forcaSenha = document.querySelector(".forca");
 
 botoes[0].onclick = diminuiTamanho;
 botoes[1].onclick = aumentaTamanho;
